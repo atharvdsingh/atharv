24e49e0183cc9fbae2000b8d7b953720a2fe5f6d
atharvdsingh <dsingh9450492600@gmail.com>
2026-04-08 19:20:17 +0530

    feat:changed the metadata from generated to simple generated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Hanken_Grotesk } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -9,8 +8,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Footer from "@/components/common/Footer";
 import { Analytics } from '@vercel/analytics/next';
 import GoToTop from "@/components/common/GoToTop";
-import { generateMetadata as getMetadata } from "@/config/Meta.config";
+// import { generateMetadata as getMetadata } from "@/config/Meta.config";
 import Blur from "@/components/common/Blur";
+import  type {Metadata}  from "next";
 
 const hanken_Groktesk = Hanken_Grotesk({
   subsets: ["latin"],
@@ -18,8 +18,44 @@ const hanken_Groktesk = Hanken_Grotesk({
   variable: "--font-hanken",
 });
 
-export const metadata = getMetadata('/');
+export const metadata: Metadata = {
+  title: {
+    default: "Atharv dev singh full stack developer ",
+    template: "%s | Microveda Lab",
+  },
+  description:
+    "Atharv dev singh i'm a full stack developer based in india, i worked as a full stack developer ",
 
+  keywords: [
+    "Atharv Dev Singh",
+    "University of Allahabad",
+    "Full stack ",
+    "Devops ",
+    "Atharv",
+    "Atharv dev ",
+    "Backend",
+    "Sanchetna",
+    "Atharv Sanchetna",
+  ],
+
+  authors: [{ name: "Atharv Dev Singh" }],
+
+  metadataBase: new URL("https://atharv.sanchetna.in"),
+
+  openGraph: {
+    title: "Atharv Dev singh",
+    description:
+    "Atharv dev singh i'm a full stack developer based in india, i worked as a full stack developer ",
+    url: "https://atharv.sanchetna.in",
+    siteName: "Atharv Dev Singh",
+    type: "website",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 export default function RootLayout({
   children,
 }: Readonly<{
